705f055a51d884b58b5a19d4e9fef794fea2c9ad
brickly26 <sabburak26@gmail.com>
2022-07-09 20:33:50 -0400

    added 4 new component files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React from "react";
-import { Button, styled, Typography } from '@mui/material';
-import {Add, Settings } from '@mui/icons-material';
+import { Box, Container } from "@mui/material";
+
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
 
 const App = () => {
-
-    const BlueButton = styled(Button)({
-        backgroundColor: 'skyblue',
-          color: '#888',
-          margin: 5,
-          "&:hover": {
-              backgroundColor: "lightblue"
-          },
-          "&:disabled": {
-              background: 'gray',
-              color: 'white'
-          }
-    });
   return (
-    <div>
-      <Button variant="text">Text</Button>
-      <Button startIcon={<Settings />} variant="contained" color="success" size="small">Settings</Button>
-      <Button startIcon={<Add/>} variant="outlined" disabled>Add new post</Button>
-      <Typography variant="h1" >
-          It uses h1 style but its a p tag
-      </Typography>
-      <BlueButton>My Button</BlueButton>
-      <BlueButton>Another Button</BlueButton>
-    </div>
+    <Container
+      sx={{
+        backgroundColor: "red",
+      }}
+    >
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+    </Container>
   );
 };
 
